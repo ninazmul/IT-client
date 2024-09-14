9392e168c98dd5d43b5fc64867293d9fd644d190
ninazmul <nazmulsaw@gmail.com>
2024-09-14 20:16:55 +0600

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,34 @@
 "use client";
+
 import "./globals.css";
-import { Poppins } from "next/font/google";
-import { Josefin_Sans } from "next/font/google";
-import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
-import React, { useEffect } from "react";
-import socketIO from "socket.io-client";
-import { Providers } from "./Provider";
+import { Poppins, Josefin_Sans } from "next/font/google";
 import { ThemeProvider } from "./utils/theme-provider";
+import { Toaster } from "react-hot-toast";
+import { Providers } from "./Provider";
+import { SessionProvider } from "next-auth/react";
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 import Loader from "./components/Loader/Loader";
+import { useEffect } from "react";
+import socketIO, { Socket } from "socket.io-client";
+
 const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
-const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
+const socket: Socket = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-Poppins",
 });
+
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-Josefin",
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${poppins.variable} ${josefin.variable} bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-[#0d0141] dark:to-[#0d0523] duration-300 min-h-screen`}
       >
@@ -48,11 +46,14 @@ export default function RootLayout({
 }
 
 const Custom: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { isLoading } = useLoadUserQuery({});
+  const { isLoading } = useLoadUserQuery(undefined, {});
 
   useEffect(() => {
-    socketId.on("connection", () => {});
+    socket.on("connection", () => {});
+    return () => {
+      socket.off("connection");
+    };
   }, []);
 
-  return <>{isLoading ? <Loader /> : <>{children}</>}</>;
+  return <>{isLoading ? <Loader /> : children}</>;
 };
